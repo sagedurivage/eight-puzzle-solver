5e8236e0bb60d571a555d90c3d1a3507a089c2d5
sduri001 <sduri001@ucr.edu>
2024-10-31 20:25:02 -0700

    Puzzle Class Implementation - changed problem class in coedbase frameowrk to puzzle; sounded better - declared private variables, class constructor & member accessor functions in header - defined respective class items in source file

---
+++ src/puzzle.cpp
@@ -0,0 +1,17 @@
+
+#include "../include/puzzle.h"
+
+// constructinator definition
+Puzzle::Puzzle(const vector<int> & init, const vector<int> & goal)
+    : init(init), goal(goal)
+{
+}
+
+/* funky definitions */
+
+const vector<int> & Puzzle::getInitial() const { return init; }
+
+const vector<int> & Puzzle::getGoal() const { return goal; }
+
+bool Puzzle::solved(const vector<int> state) const { return state == goal; }    // == operator for vector
+
