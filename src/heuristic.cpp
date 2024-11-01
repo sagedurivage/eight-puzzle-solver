d975bb5dfd5cb67cf85621669d1c18b5ae8cc75a
sduri001 <sduri001@ucr.edu>
2024-11-01 11:51:00 -0700

    Heuristic Class Update - defined Misplaced Tile Heuristic

--- src/heuristic.cpp
+++ src/heuristic.cpp
@@ -2,20 +2,33 @@
 #include "../include/heuristic.h"
 #include <cmath>
 
-// Uniform Cost is A* with heuristic of zero
+// Uniform Cost is A* with heuristic of zero (from project outline note)
 int Heuristic::uniformCost(const Node & node, const Puzzle & puzzle) { return 0; }
 
 // Count number of tiles in current state NOT matching goal state
 int Heuristic::misplacedTile(const Node & node, const Puzzle & puzzle)
 {
-    // FIXME
-    return 1;
+    int cnt = 0;                                    // count
+    const vector<int> & curr = node.getState();     // access (ref to) current node's state
+    const vector<int> & goal = puzzle.getGoal();    // know (ref to) where we're going
+
+    // iterate through ea. number of puzzle & increment count for ea. NOT matching position 
+    // in goal state
+    for (size_t i = 0; i < curr.size(); ++i)
+    {
+        if (curr[i] != goal[0] && curr[i] != 0)
+        {
+            cnt++;
+        }
+    }
+
+    return cnt;
 }
 
 // Let's get hypotenuse!
 int Heuristic::euclideanDist(const Node & node, const Puzzle & puzzle)
 {
-    // FIXME
+    // FIXME bruh idk rn
     return 1;
 }
 
