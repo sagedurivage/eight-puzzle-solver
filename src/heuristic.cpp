dd95422a3811f63c0018821cbe9c4cacd55ebb48
sduri001 <sduri001@ucr.edu>
2024-11-01 20:59:01 -0700

    Heuristic Class Update - implemented euclidean distance heuristic

--- src/heuristic.cpp
+++ src/heuristic.cpp
@@ -28,9 +28,36 @@ int Heuristic::misplacedTile(const Node & node, const Puzzle & puzzle)
 }
 
 // Let's get hypotenuse!
+// formula source: https://en.wikipedia.org/wiki/Euclidean_distance (2-dimensional)
 int Heuristic::euclideanDist(const Node & node, const Puzzle & puzzle)
 {
-    // FIXME bruh idk rn
-    return 1;
+    int dist = 0;
+    const vector<int> & curr = node.getState();     // access (ref to) current node's state
+    const vector<int> & goal = puzzle.getGoal();    // know (ref to) where we're going
+
+    // iterate through each element of current puzzle state
+    for (size_t i = 0; i < curr.size(); ++i)
+    {
+        if (curr[i] != 0)   // disregard empty space in puzzle
+        {
+            // identify index of element in solution
+            int goalIndex = 0;
+            while (curr[i] != goal[goalIndex])
+            {
+                ++goalIndex;
+            }
+
+            // get positions on puzzle grid
+            int currRow = i / 3;    // rows 0, 1, 2
+            int currCol = i % 3;    // columns 0, 1, 2
+            int goalRow = goalIndex / 3;
+            int goalCol = goalIndex % 3;
+            // calculate euclidean distance between indices
+            dist += sqrt(((currRow - goalRow) * (currRow - goalRow)) +
+                         ((currCol - goalCol) * (currCol - goalCol)));
+        }
+    }
+
+    return dist;
 }
 
